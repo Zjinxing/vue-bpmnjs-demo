ebf1f5f12ce7e960c2ac27738386f110ac2aeb1d
zhaojinxing <1182999456@qq.com>
2019-04-27 15:18:56 +0800

    翻译为中文

--- src/translate/customerTranslate.js
+++ src/translate/customerTranslate.js
@@ -1,8 +1,20 @@
 import translations from './translation'
 
+// bpmn-js-i18n 中未列出的
+const extraTranslate = {
+  StartEvent: '开始事件',
+  EndEvent: '结束事件',
+  Task: '任务',
+  DataObjectReference: '数据对象',
+  DataStoreReference: '数据存储库'
+}
 export default function customTranslate (template, replacements) {
   replacements = replacements || {}
 
   template = translations[template] || template
-  return template.replace(/{([^}]+)}/g, (_, key) => replacements[key] || `{${key}}`)
+  return template.replace(/{([^}]+)}/g, (_, key) => {
+    console.log(replacements[key])
+
+    return extraTranslate[replacements[key]] || `{${key}}`
+  })
 }
